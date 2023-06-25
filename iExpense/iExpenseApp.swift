dbced87c66bb21ff2835cf9546dd4e00c6134389
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-25 12:16:43 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Khumbongmayum Tonny on 25/06/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
