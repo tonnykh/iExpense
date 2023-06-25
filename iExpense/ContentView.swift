d15d184ee6817403a75ee0c78d46accb4199dbf8
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-06-25 15:30:35 +0530

    Create expenseItem to represent single expense

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,15 +7,10 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello")
     }
 }
 
---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Khumbongmayum Tonny on 25/06/23.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: String
+}
